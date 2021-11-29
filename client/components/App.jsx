caece4ef5624f98216890333fe2e1f01e41ab05d
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-29 16:28:59 +1300

    added landing page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+
+
+// Components
+import Landing from './Landing'
 
 function App () {
 
   return (
     <>
       <div className='app'>
-    
+        <Route exact path='/' component={Landing} />
       </div>
     </>
   )
