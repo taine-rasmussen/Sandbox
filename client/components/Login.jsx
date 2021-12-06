5a0807cc37e0b0ec63ddf5d1fa6a8f31afe3b440
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-12-06 15:38:39 +1300

    added home btn to login page

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import '../styles/Login.css'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
    return (
       <div className='login-container'>
+
+         <Link to='/'>
+            <button>Home</button>
+         </Link>
+
           <form className="login-form">
             <input 
                type="text"
