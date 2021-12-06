3c706042d544c504085023099150f098a51f831c
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-12-06 15:27:20 +1300

    added form and inputs

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Landing = () => {
    return (
-      <div>
+      <div className="landing-container">
          <h1>Landing page</h1>
+
+         <form>
+            <input 
+               type="text"
+               placeholder="Username..."
+            />
+            <input 
+               type="password"
+               placeholder="Password..."
+            />
+
+         </form>
       </div>
    )
 }
